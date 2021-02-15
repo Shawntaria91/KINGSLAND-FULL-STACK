7f0fc00d62b213294a786a142a48ffb05dd061f1
shawntaria91 <shawntariaburden1991@gmail.com>
2021-02-14 20:11:26 -0500

    completed reg form

---
+++ Q1 MODULES/TOPIC 0.1 HTML/EXERCISES/regForm.js
@@ -0,0 +1,11 @@
+const inputs = document.querySelectorAll("input, select, textarea");
+
+inputs.forEach((input) => {
+	input.addEventListener(
+		"invalid",
+		(event) => {
+			input.classList.add("error");
+		},
+		false
+	);
+});
