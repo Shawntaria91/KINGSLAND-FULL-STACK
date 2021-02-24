667d4b3aafcc6d758f3a87beeeb7120416c37ae2
shawntaria91 <shawntariaburden1991@gmail.com>
2021-02-23 21:39:31 -0500

    resolved calc error

--- Programming Basics/EXERCISE SIX CONDITIONAL STATEMENTS/metricConverter.js
+++ Programming Basics/EXERCISE SIX CONDITIONAL STATEMENTS/metricConverter.js
@@ -12,7 +12,7 @@ function main(num, input, output) {
 			convertedUnit = num * 1000;
 			console.log(convertedUnit.toFixed(10));
 		} else if (output == "cm") {
-			converteUnit = num * 100;
+			convertedUnit = num * 100;
 			console.log(convertedUnit.toFixed(10));
 		} else if (output == "mi") {
 			convertedUnit = num * 0.000621371192;
@@ -30,14 +30,14 @@ function main(num, input, output) {
 			convertedUnit = num * 1.0936133;
 			console.log(convertedUnit.toFixed(10));
 		} else {
-			console.log("Already in desired metric unit");
+			console.log(num.toFixed(10));
 		}
 	} else if (input == "mm") {
 		if (output == "m") {
 			convertedUnit = num * (1 / 1000);
 			console.log(convertedUnit.toFixed(10));
 		} else if (output == "cm") {
-			converteUnit = num * (1 / 1000) * 100;
+			convertedUnit = num * (1 / 1000) * 100;
 			console.log(convertedUnit.toFixed(10));
 		} else if (output == "mi") {
 			convertedUnit = num * (1 / 1000) * 0.000621371192;
@@ -55,14 +55,14 @@ function main(num, input, output) {
 			convertedUnit = num * (1 / 1000) * 1.0936133;
 			console.log(convertedUnit.toFixed(10));
 		} else {
-			console.log("Already in desired metric unit");
+			console.log(num.toFixed(10));
 		}
 	} else if (input == "cm") {
 		if (output == "mm") {
 			convertedUnit = num * (1 / 100) * 1000;
 			console.log(convertedUnit.toFixed(10));
 		} else if (output == "m") {
-			converteUnit = num * (1 / 100);
+			convertedUnit = num * (1 / 100);
 			console.log(convertedUnit.toFixed(10));
 		} else if (output == "mi") {
 			convertedUnit = num * (1 / 100) * 0.000621371192;
@@ -80,14 +80,14 @@ function main(num, input, output) {
 			convertedUnit = num * (1 / 100) * 1.0936133;
 			console.log(convertedUnit.toFixed(10));
 		} else {
-			console.log("Already in desired metric unit");
+			console.log(num.toFixed(10));
 		}
 	} else if (input == "mi") {
 		if (output == "mm") {
 			convertedUnit = num * (1 / 0.000621371192) * 1000;
 			console.log(convertedUnit.toFixed(10));
 		} else if (output == "cm") {
-			converteUnit = num * (1 / 0.000621371192) * 100;
+			convertedUnit = num * (1 / 0.000621371192) * 100;
 			console.log(convertedUnit.toFixed(10));
 		} else if (output == "m") {
 			convertedUnit = num * (1 / 0.000621371192);
@@ -105,14 +105,14 @@ function main(num, input, output) {
 			convertedUnit = num * (1 / 0.000621371192) * 1.0936133;
 			console.log(convertedUnit.toFixed(10));
 		} else {
-			console.log("Already in desired metric unit");
+			console.log(num.toFixed(10));
 		}
 	} else if (input == "in") {
 		if (output == "mm") {
 			convertedUnit = num * (1 / 39.3700787) * 1000;
 			console.log(convertedUnit.toFixed(10));
 		} else if (output == "cm") {
-			converteUnit = num * (1 / 39.3700787) * 100;
+			convertedUnit = num * (1 / 39.3700787) * 100;
 			console.log(convertedUnit.toFixed(10));
 		} else if (output == "m") {
 			convertedUnit = num * (1 / 39.3700787);
@@ -130,14 +130,14 @@ function main(num, input, output) {
 			convertedUnit = num * (1 / 39.3700787) * 1.0936133;
 			console.log(convertedUnit.toFixed(10));
 		} else {
-			console.log("Already in desired metric unit");
+			console.log(num.toFixed(10));
 		}
 	} else if (input == "km") {
 		if (output == "mm") {
-			convertedUnit = num * (1 / 0.001);
+			convertedUnit = num * (1 / 0.001) * 1000;
 			console.log(convertedUnit.toFixed(10));
 		} else if (output == "cm") {
-			converteUnit = num * (1 / 0.001) * 100;
+			convertedUnit = num * (1 / 0.001) * 100;
 			console.log(convertedUnit.toFixed(10));
 		} else if (output == "mi") {
 			convertedUnit = num * (1 / 0.001) * 0.000621371192;
@@ -155,14 +155,14 @@ function main(num, input, output) {
 			convertedUnit = num * (1 / 0.001) * 1.0936133;
 			console.log(convertedUnit.toFixed(10));
 		} else {
-			console.log("Already in desired metric unit");
+			console.log(num.toFixed(10));
 		}
 	} else if (input == "ft") {
 		if (output == "mm") {
 			convertedUnit = num * (1 / 3.2808399) * 1000;
 			console.log(convertedUnit.toFixed(10));
 		} else if (output == "cm") {
-			converteUnit = num * (1 / 3.2808399) * 100;
+			convertedUnit = num * (1 / 3.2808399) * 100;
 			console.log(convertedUnit.toFixed(10));
 		} else if (output == "mi") {
 			convertedUnit = num * (1 / 3.2808399) * 0.000621371192;
@@ -180,14 +180,14 @@ function main(num, input, output) {
 			convertedUnit = num * (1 / 3.2808399) * 1.0936133;
 			console.log(convertedUnit.toFixed(10));
 		} else {
-			console.log("Already in desired metric unit");
+			console.log(num.toFixed(10));
 		}
 	} else if (input == "yd") {
 		if (output == "mm") {
 			convertedUnit = num * (1 / 1.0936133) * 1000;
 			console.log(convertedUnit.toFixed(10));
 		} else if (output == "cm") {
-			converteUnit = num * (1 / 1.0936133) * 100;
+			convertedUnit = num * (1 / 1.0936133) * 100;
 			console.log(convertedUnit.toFixed(10));
 		} else if (output == "mi") {
 			convertedUnit = num * (1 / 1.0936133) * 0.000621371192;
@@ -205,7 +205,7 @@ function main(num, input, output) {
 			convertedUnit = num * (1 / 1.0936133) * 3.2808399;
 			console.log(convertedUnit.toFixed(10));
 		} else {
-			console.log("Already in desired metric unit");
+			console.log(num.toFixed(10));
 		}
 	} else {
 		console.log(
@@ -213,4 +213,4 @@ function main(num, input, output) {
 		);
 	}
 }
-main(150, "mi", "in");
+main(94308, "km", "mm");
