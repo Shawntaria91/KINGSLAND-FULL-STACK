e9bc25f337bb3e38a09d3264a7bc176e8e7e4e67
shawntaria91 <shawntariaburden1991@gmail.com>
2021-02-24 19:07:23 -0500

    resolved misspelled flower name

--- Programming Basics/EXERCISE 9 COND STATEMENTS ADVANCED/newHouse.js
+++ Programming Basics/EXERCISE 9 COND STATEMENTS ADVANCED/newHouse.js
@@ -1,44 +1,50 @@
 function main(flower, numFlowers, budget) {
-    var price = 0.00;
+	var price = 0.0;
 
-    if (flower == "Roses") {
-        price = numFlowers * 5;
-        if (numFlowers > 80) {
-            price = price - (price * 0.10);
-        } else {  
-        }
-    } else if (flower == "Dhalias") {
-        price = numFlowers * 3.80;
-        if (numFlowers > 90) {
-            price = price - (price * 0.15);
-        } else {
-        }
-    } else if (flower == "Tulips") {
-        price = numFlowers * 2.80;
-        if (numFlowers > 80) {
-            price = price - (price * 0.15);
-        } else {
-        }
-    } else if (flower == "Narcissus") {
-        price = numFlowers * 3;
-        if (numFlowers < 120) {
-            price = price + (price * 0.15);
-        } else {
-        }
-    } else if (flower == "Gladiolus") {
-        price = numFlowers * 2.50;
-        if (numFlowers < 80) {
-            price = price + (price * 0.20);
-        } else {
-        }
-    }
+	if (flower == "Roses") {
+		price = numFlowers * 5;
+		if (numFlowers > 80) {
+			price = price - price * 0.1;
+		} else {
+		}
+	} else if (flower == "Dahlias") {
+		price = numFlowers * 3.8;
+		if (numFlowers > 90) {
+			price = price - price * 0.15;
+		} else {
+		}
+	} else if (flower == "Tulips") {
+		price = numFlowers * 2.8;
+		if (numFlowers > 80) {
+			price = price - price * 0.15;
+		} else {
+		}
+	} else if (flower == "Narcissus") {
+		price = numFlowers * 3;
+		if (numFlowers < 120) {
+			price = price + price * 0.15;
+		} else {
+		}
+	} else if (flower == "Gladiolus") {
+		price = numFlowers * 2.5;
+		if (numFlowers < 80) {
+			price = price + price * 0.2;
+		} else {
+		}
+	}
 
-    var moneyLeft = budget - price;
-    var moneyNeeded = price - budget;
-    if (price > budget) {
-        console.log(`Not enough money, you need ${moneyNeeded.toFixed(2)} leva more.`);
-    } else {
-        console.log(`Hey, you have a great garden with ${numFlowers} ${flower} and ${moneyLeft.toFixed(2)} leva left.`);
-    }
+	var moneyLeft = budget - price;
+	var moneyNeeded = price - budget;
+	if (price > budget) {
+		console.log(
+			`Not enough money, you need ${moneyNeeded.toFixed(2)} leva more.`
+		);
+	} else {
+		console.log(
+			`Hey, you have a great garden with ${numFlowers} ${flower} and ${moneyLeft.toFixed(
+				2
+			)} leva left.`
+		);
+	}
 }
-main("Dhalias", 603, 1002);
+main("Roses", 79, 1002);
